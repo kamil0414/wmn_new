d0c00f2879b330858a29a0d900190efa9099f3d9
kamil0414@gmail.com <kamil0414@gmail.com>
2023-03-10 14:33:07 +0100

    fix: auth

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ['/', '/index'],
-}
+  matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+};
 
 export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization')
-  const url = req.nextUrl
+  const basicAuth = req.headers.get("authorization");
+  const url = req.nextUrl;
 
   if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1]
-    const [user, pwd] = atob(authValue).split(':')
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
 
     if (user === process.env.USER && pwd === process.env.PASSWORD) {
-      return NextResponse.next()
+      return NextResponse.next();
     }
   }
-  url.pathname = '/api/auth'
+  url.pathname = "/api/auth";
 
-  return NextResponse.rewrite(url)
+  return NextResponse.rewrite(url);
 }
