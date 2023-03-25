6369492848e9933a2454c62f74149fe2c6c62bef
kamil0414 <kamil0414@gmail.com>
2023-03-25 18:54:02 +0100

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,8 +5,8 @@ export default function Document() {
     <Html lang="pl-PL">
       <Head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="favicon.ico" />
-        <link rel="manifest" href="manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <meta name="description" content="WMN" />
       </Head>
