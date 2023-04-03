fa5cba573ad4acc0cdedf72634f3d966c21bc602
kamil0414@gmail.com <kamil0414@gmail.com>
2023-04-03 09:20:57 +0200

    fix: flat hostory

--- pages/api/flatHistory.ts
+++ pages/api/flatHistory.ts
@@ -31,7 +31,7 @@ export default async function handler(
           {
             data: {
               gte: getStartDateFromEnv(),
-              lte: getEndDateFromEnv(),
+              lte: new Date(Math.max(getEndDateFromEnv().setDate(10), getEndDateFromEnv().getTime()))
             },
           },
         ],
