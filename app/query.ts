ce263e556e5c5f1ca9c7447412e3023a0f7b211f
kamil0414@gmail.com <kamil0414@gmail.com>
2024-01-01 16:36:56 +0100

    chore: refactor

--- app/query.ts
+++ app/query.ts
@@ -1,12 +1,12 @@
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
 
-const incorrectDescriptions = async () =>
-  prisma.niepoprawny_opis.findFirst({
-    select: {
-      zaczyna_sie_od: true,
-      zawiera: true,
-      konczy_sie_na: true,
-    },
-  });
+const incorrectDescriptions = async () => [];
+// prisma.niepoprawny_opis.findFirst({
+//   select: {
+//     zaczyna_sie_od: true,
+//     zawiera: true,
+//     konczy_sie_na: true,
+//   },
+// });
 
 export default incorrectDescriptions;
